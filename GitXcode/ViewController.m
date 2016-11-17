8fad145f4ef69d469ef548a64cef4d1608e3e037
JerryC <pride508@gmail.com>
2016-11-17 20:15:03 +0800

    Initial Commit

---
+++ GitXcode/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GitXcode
+//
+//  Created by Chuang Jerry on 2016/11/17.
+//  Copyright © 2016年 Chuang Jerry. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
